465643c8954f99d6dba80e15d62af79ab1099838
Aniol Rodríguez <aniolrodriguez2005@gmail.com>
2026-05-11 11:06:55 +0200

    actualitzar aniol

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 
 import App from "./App.jsx";
@@ -8,32 +9,40 @@ import Login from "./pages/login/login.jsx";
 import Regist from "./pages/regist/regist.jsx";
 import Destinacio from "./pages/destinacio/destinacio.jsx";
 import Perfil from "./pages/perfil/perfil.jsx";
+import PerfilConvidat from "./pages/perfilconvidat/perfilconvidat.jsx";
 import Serveis from "./pages/serveis/serveis.jsx";
 import Mapa from "./pages/mapa/mapa.jsx";
 
 import Home from "./pages/home/home.jsx";
-
-import PrivateRoute from "./private/PrivateRoute";
-import PublicRoute from "./private/PublicRoute";
 import ForgotPassword from "./pages/forgotpassword/forgotpassword.jsx";
+
+import PrivateRoute from "./private/PrivateRoute.jsx";
+import PublicRoute from "./private/PublicRoute.jsx";
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PublicRoute><App /></PublicRoute>} />
-        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-        <Route path="/regist" element={<PublicRoute><Regist /></PublicRoute>} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-           {/* Privadas (si NO hay token → login) */}
-        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
-        <Route path="/mapa" element={<PrivateRoute><Mapa /></PrivateRoute>} />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PublicRoute><App /></PublicRoute>} />
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/regist" element={<PublicRoute><Regist /></PublicRoute>} />
+          <Route path="/forgot-password" element={<PublicRoute><ForgotPassword /></PublicRoute>} />
 
-        <Route path="/destinacio" element={<PrivateRoute><Destinacio /></PrivateRoute>} />
-        <Route path="/perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
-        <Route path="/serveis" element={<PrivateRoute><Serveis /></PrivateRoute>} />
-        <Route path="*" element={<h1>404</h1>} />
-      </Routes>
-    </BrowserRouter>
+          {/* PRIVADES */}
+          <Route path="/home" element={<PrivateRoute allowGuest><Home /></PrivateRoute>} />
+          <Route path="/mapa" element={<PrivateRoute allowGuest><Mapa /></PrivateRoute>} />
+          <Route path="/destinacio" element={<PrivateRoute allowGuest><Destinacio /></PrivateRoute>} />
+          <Route path="/serveis" element={<PrivateRoute allowGuest><Serveis /></PrivateRoute>} />
+          <Route path="/perfil-convidat" element={<PrivateRoute allowGuest><PerfilConvidat /></PrivateRoute>} />
+
+          <Route path="/perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
+
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
