42ab504822ff5e53ba91f19aa8c692bb42ef476d
Aniol Rodríguez <aniolrodriguez2005@gmail.com>
2026-05-14 17:12:04 +0200

    import mapa

--- frontend/src/pages/mapa/mapa.jsx
+++ frontend/src/pages/mapa/mapa.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
-import { useLocation, MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from "react-leaflet";
+import { useLocation } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./mapa.css";
